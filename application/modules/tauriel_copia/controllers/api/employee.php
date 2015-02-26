12d62f2ec729446e8ae7af8aa1abfff33521624b
root <root@CrunchBang.(none)>
2015-02-26 18:57:20 +0100

    OK

---
+++ application/modules/tauriel_copia/controllers/api/employee.php
@@ -0,0 +1,142 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Example
+ *
+ * This is an example of a few basic user interaction methods you could use
+ * all done with a hardcoded array.
+ *
+ * @package     CodeIgniter
+ * @subpackage  Rest Server
+ * @category    Controller
+ * @author      Phil Sturgeon
+ * @link        http://philsturgeon.co.uk/code/
+*/
+
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+require APPPATH.'/libraries/REST_Controller.php';
+
+class employee extends REST_Controller {
+
+    //public $LOGTAG = "EBRE_ESCOOL API: ";
+
+    function __construct() {
+        // Construct our parent class
+        parent::__construct();
+        
+        // Configure limits on our controller methods. Ensure
+        // you have created the 'limits' table and enabled 'limits'
+        // within application/config/rest.php
+        $this->methods['employee_get']['limit'] = 500; //500 requests per hour per user/key
+        $this->methods['employee_post']['limit'] = 100; //100 requests per hour per user/key
+        $this->methods['employee_delete']['limit'] = 50; //50 requests per hour per user/key
+        $this->methods['employee_put']['limit'] = 10; //10 requests per hour per user/key
+
+        //Loading "employees_model.php" model.
+        $this->load->model('api_employees');
+    }    
+
+    public function index_get(){
+        $this->employee_get();
+    }
+
+    function employee_get(){
+
+        if(!$this->get('id')){
+            //$message = array('id'=>'','message'=>'YOU MUST SEND AND ID');
+            $this->response($message, 400);
+        }
+
+        //$this->load->model('employees');
+
+        //$employee = $this->api_employees->employees_model->getEmployee( $this->get('id') );
+        $employee = $this->api_employees->getEmployee( $this->get('id') );
+
+        if($employee){
+            $this->response($employee, 200); // 200 being the HTTP response code
+        } else {
+            //$this->response(array('id' => $this->get('id'),'message' => 'employee not exists!'), 404);
+            $this->response(array('error' => 'employee could not be found'), 404);
+        }
+    }
+
+    function employees_get(){
+
+        //$this->load->model('api_employees');
+        //$employees = $this->api_employees->employees_get();
+        $employee = $this->api_employees->getEmployees();
+    
+        if($employee){
+            $this->response($employee, 200); // 200 being the HTTP response code
+        } else {
+            $this->response(array(array('id' => $this->get('id'),'message' => 'Couldn\'t find any employees!'), 404);
+        }
+    }
+    
+    function employee_post(){
+
+        if (isset($_POST)){
+            //GET DATA FROM POST
+            $id = $this->input->get_post('id');
+            $data = array('employees_id'=>$this->input->get_post('employees_id'));
+            //CALL TO MODEL
+            $response = $this->api_employees->updateEmployees($id,$data);
+        }
+
+        if($response){
+            $message = array('id' => $id, 'message' => 'UPDATED!');
+            $this->response($message, 200); // 200 being the HTTP response code
+        } else{
+            $message = array('id' =>$id, 'message' => 'ERROR UPDATING!');
+            $this->response($message, 404); // 404 being the HTTP response code(Not Found)
+        }
+        //$message = array('id' => $this->get('id'), 'name' => $this->post('name'), 'email' => $this->post('email'), 'message' => 'ADDED!');
+    }
+
+     function employee_delete(){
+        
+        if(!$this->get('id')){
+            $message = array('id'=>'','message'=>'YOU MUST SEND AN ID');
+            $this->response($message, 400);
+        }
+
+        $response = $this->api_employees->deleteEmployees( $this->get('id') );
+        
+        if($response){
+            $message = array('id' => $this->get('id'), 'message' => 'DELETED!');
+            $this->response($message, 200); // 200 being the HTTP response code
+        } else{
+            $message = array('id' => $this->get('id'), 'message' => 'ERROR DELETING!');
+            $this->response($message, 404); // 400 being the HTTP response code(Not Found)
+        }
+    }
+
+    function employee_put(){
+
+        $data = array(
+        'employees_id'=>$this->put('employees_id'),
+        'employees_person_id'=>$this->put('employees_person_id'),
+        'employees_code'=>$this->put('employees_code'),
+        'employees_type'=>$this->put('employees_type_id'));
+        
+        $insertResponse = $this->api_employees->insertEmployees($data);
+        //echo $insertResponse['response']." ".$insertResponse['id'];
+        
+        if($insertResponse['response']){
+            $message = array('id' => $insertResponse['id'],'message' => 'NEW CLASSROOM GROUP INSERTED');
+            $this->response($message,200);//200 being the HTTP response code
+        } else{
+            $message = array('id' => $insertResponse['id'], 'message' => 'ERROR INSERTING');
+            $this->response($message, 422); // 422 being the HTTP response code
+        }
+    }
+
+    public function send_post() {
+        var_dump($this->request->body);
+    }
+
+
+    public function send_put() {
+        var_dump($this->put('foo'));
+    }
+}
---
+++ application/modules/tauriel_copia/controllers/api/employees_api_test.php
@@ -0,0 +1,79 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+* Persons API
+*
+* @package Ebre-escool
+* @subpackage API
+* @category Controller
+* @author Paolo Davila
+* @link http://acacha.org/meidawiki/index.php/ebre-escool
+* @editedBy Turcan Nicolae
+*/
+
+class ebreescool_api_test extends CI_Controller{
+	function __construct(){
+		parent::__construct();
+		
+		// Load the library
+		$this->load->library('REST');
+		$this->load->config('rest_client');
+
+		// Set config options (only 'server' is required to work)
+		//http://localhost/ebre-escool/index.php/tauriel/api/employees/employee/id/1
+		$config = array('server' => 'http://localhost/ebre-escool/index.php/tauriel/api/employees/',
+			'api_key' => $this->config->item('api_key'),
+			'api_name' => 'X-API-KEY',
+			//'http_user' => 'username',
+			//'http_pass' => 'password',
+			//'http_auth' => 'basic',
+			//'ssl_verify_peer' => TRUE,
+			//'ssl_cainfo' => '/certs/cert.pem'
+			);
+
+		// Run some setup
+		$this->rest->initialize($config);
+	}
+
+	public function index(){
+	// Pull in an array of tweets
+		$employee = $this->rest->get('employee/id/1');
+		echo json_encode($employee);
+	}
+
+	public function employee_post(){
+	// Pull in an array of tweets
+		$givenName = "Paolo";
+		$sn1 = "Davila";
+		$email1 = "paolodavila@iesebre.com";
+
+		//...
+		$post_array = array(
+			"givenName" => $givenName,
+			"sn1" => $sn1,
+			"email1" => $email1
+			//other field...
+			);
+
+			// Pull in an array of tweets
+		$result = $this->rest->post('employee',$post_array);
+
+		echo "<br> STATUS CODE: " . $result = $this->rest->status() . "</br>";
+		
+		$result = $this->rest->debug();
+		
+		echo json_encode($result);
+	}
+
+	public function test_login(){
+		$username = "paolodavila";
+		$password = "PUT_YOUR_PASSWORD_HERE";
+		$realm = "ldap";
+		$post_array = array("username" => $username, "password" => $password, "realm" => $realm);
+		// Pull in an array of tweets
+		$result = $this->rest->post('login',$post_array);
+		echo "<br> STATUS CODE: " . $result = $this->rest->status() . "</br>";
+		$result = $this->rest->debug();
+		echo json_encode($result);
+	}
+}
