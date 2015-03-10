65ae31e4db6a2eb82ba359f7b5f44ae30fcc1d88
root <root@CrunchBang.(none)>
2015-03-10 11:49:59 +0100

    Inventari arreglat per cap de departaments i cap d'estudis com a admin

--- application/modules/inventory/controllers/inventory.php
+++ application/modules/inventory/controllers/inventory.php
@@ -291,9 +291,13 @@ public function inventory_object($organizational_unit="")	{
 	//TODO
 	$user_is_admin = $this->ebre_escool->user_is_admin();
 
+	$user_is_inventory = $this->ebre_escool->user_is_inventory();           // new users for inventory
+
 	$data['user_is_admin'] = $user_is_admin;
 
-	if ($user_is_admin) {
+	$data['user_is_inventory'] = $user_is_inventory;						// new users for inventory
+
+	if (($user_is_admin) || ($user_is_inventory))  {
 		$all_organizational_units = $this->inventory_model->getAllorganizationalUnits();
 		$organizational_units = $all_organizational_units;
 	} else {
@@ -302,7 +306,7 @@ public function inventory_object($organizational_unit="")	{
 	
 	$data['organizational_units'] = $organizational_units;
 
-	if ( $organizational_unit != "" && $user_is_admin) {
+	if (($organizational_unit != "" && $user_is_admin) || ($organizational_unit != "" && $user_is_inventory)) {
 		$data['selected_organizational_unit'] = $selected_organizational_unit;
 		$data['selected_organizational_unit_key'] = $organizational_unit;
 	} else {
